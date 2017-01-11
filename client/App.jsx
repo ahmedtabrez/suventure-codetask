61bf79f1d8bef4ab62e8ce59b6ec64b660e49f2a
tabrez <tabrez.tk@gmail.com>
2017-01-11 13:13:34 +0530

    On for submission

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { mount } from 'react-mounter';
 import RenderTemplate from './RenderTemplate.jsx'
 
 Meteor.startup(function(){
-	ReactDOM.render(<RenderTemplate template="loginButtons" />, document.getElementById("app"));
+	ReactDOM.render(<RenderTemplate template="loginButtons" />, document.getElementById("loginButtons"));
 	ReactDOM.render(<RenderTemplate template="allUsers" />, document.getElementById("allUsers"));
 	ReactDOM.render(<RenderTemplate template="conversation" />, document.getElementById("conversation"));
-})
+});
--- client/main.js
+++ client/main.js
@@ -6,10 +6,16 @@ import './main.html';
 Meteor.subscribe('users');
 Meteor.subscribe('messages');
 
+Meteor.startup(function(){
+	AutoForm.setDefaultTemplate('materialize');
+});
+
 Template.registerHelper('users',()=>{
 	return Meteor.users.find()
-})
-
+});
+Template.registerHelper('username', (userId)=> {
+	return Meteor.users.findOne(userId).emails[0].address;
+});
 Template.conversation.helpers({
 	messages: ()=> {
 		currentConversation = Session.get('currentConversation');
@@ -19,11 +25,8 @@ Template.conversation.helpers({
 					{to: Meteor.userId(), from: currentConversation}
 				]
 			}, {
-				sort: {sentAt: -1}
+				sort: {sentAt: 1}
 			})
-	},
-	username: (userId)=> {
-		return Meteor.users.findOne(userId).emails[0].address;
 	},
 	currentConversation: (userId)=> {
 		return Session.get('currentConversation')
@@ -40,7 +43,7 @@ Template.conversation.helpers({
 		if(!Meteor.userId())
 			return "You are not signed in <br> Please sign in to send/view messages.";
 		else if(!Session.get('currentConversation'))
-			return "Select a user above to send message"
+			return "Select a registered user to send message"
 		else
 			return false
 	}
@@ -52,10 +55,15 @@ Template.allUsers.events({
 	}
 })
 
+Template.chatbox.onRendered(function(){
+	$("#chatbox").scrollTop($("#chatbox")[0].scrollHeight);
+})
+
 AutoForm.hooks({
   sendMsg: {
     onSubmit: function (doc) {
       if (Messages.insert(doc)) {
+      	$("#chatbox").scrollTop($("#chatbox")[0].scrollHeight);
         this.done();
       } else {
         this.done(new Error("Submission failed"));
